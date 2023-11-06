29efeabecc725a1bf0035fbdd0cbfdccdee30e38
Luiz Gustavo <gusstavopdm@gmail.com>
2023-11-06 17:31:55 -0300

    ✨ feat(app.module.ts): add CreateAccountController and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
+  controllers: [CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
