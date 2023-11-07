5126f4c855bab9d27ca5074410663a63914bbba7
Luiz Gustavo <gusstavopdm@gmail.com>
2023-11-07 10:32:22 -0300

    🔀 merge(app.module.ts): add AuthenticateController to the list of controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from './prisma/prisma.service'
-import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
     }),
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
